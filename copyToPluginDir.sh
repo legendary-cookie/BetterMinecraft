64dedbe76e70d47b0f54490cb8a74d6605aa5ab0
cook1e <vincent.schweiger@xolley.de>
2021-07-25 15:58:51 +0200

    reload and copy of plugin

--- copyToPluginDir.sh
+++ copyToPluginDir.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-./gradlew shadowJar
-rm -f ~/mctestserver/betterminecraft.jar
-cp build/libs/*-all.jar ~/mctestserver/betterminecraft.jar
+./gradlew clean shadowJar
+scp build/libs/Skyblock-*.jar dockeruser@docker1.getcom.de:/opt/docker/dockercraft/betterminecraft.jar
+ssh -t dockeruser@docker1.getcom.de "sudo cp /opt/docker/dockercraft/betterminecraft.jar /opt/docker/dockercraft/data/testserver/plugins/betterminecraft.jar"
+ssh -t dockeruser@docker1.getcom.de "cd /opt/docker/dockercraft && ./reload_testserver.sh"
